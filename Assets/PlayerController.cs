e0e9c58a6a4537499cf9e020292394fb6f67663c
Elias <thatsolodev@hotmail.com>
2021-05-01 00:49:08 +0200

    Started working on character movement

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float turnRate;
+
+    private CharacterController characterController;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        characterController.Move(dir * moveSpeed * Time.deltaTime);
+    }
+}
