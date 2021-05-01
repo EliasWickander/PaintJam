7e0d8f7967dcecb77ac21e069410a8ed4068473a
omegasigill <massimo.mezzalira.game@gmail.com>
2021-05-01 18:59:27 +0200

    Milk_enemy on game

--- Assets/Scripts/Enemies/Enemy_Milk.cs
+++ Assets/Scripts/Enemies/Enemy_Milk.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class Enemy_Milk : Enemy
 {
+    Animator this_anim;
+
+
+    private void Start()
+    {
+        this_anim = GetComponent<Animator>();
+    }
     protected override void Attack()
     {
         Debug.Log("milk attack!");
+        this_anim.SetTrigger("attack");
+        
+    }
+    public void Dmg_milk()
+    {
         player.ModifyHealth(-damage);
     }
 }
