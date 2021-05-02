5220cb441bf26704b60c1f5addf54acbf7167839
omegasigill <massimo.mezzalira.game@gmail.com>
2021-05-02 18:04:19 +0200

    first build

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEditor;
-using UnityEditor.Build;
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
